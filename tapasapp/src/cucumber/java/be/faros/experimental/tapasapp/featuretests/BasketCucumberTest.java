1d4e30d560a8a3f85a88c3ec65c50152918e52bf
Kristof Van Sever <vanseverk@gmail.com>
2018-04-23 20:34:02 +0200

    Added some scenarios

--- tapasapp/src/cucumber/java/be/faros/experimental/tapasapp/featuretests/BasketCucumberTest.java
+++ tapasapp/src/cucumber/java/be/faros/experimental/tapasapp/featuretests/BasketCucumberTest.java
@@ -3,20 +3,43 @@ package be.faros.experimental.tapasapp.featuretests;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import be.faros.experimental.tapasapp.BaseCucumberTest;
+import be.faros.experimental.tapasapp.store.domain.usecases.dto.TapasOrder;
 import cucumber.api.java.After;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import java.math.BigDecimal;
 
 public class BasketCucumberTest extends BaseCucumberTest {
 
-  @When("^the user creates a new basket$")
-  public void the_user_creates_a_new_basket() {
+  @When("^the user creates a new Basket$")
+  public void theUserCreatesANewBasket() {
     userBasketManagement.createNewBasket();
   }
 
-  @Then("^the number of items in the basket with id (\\d+) equals (\\d+)$")
-  public void an_empty_basket_exists_with_id(int id, int amount) {
-    assertThat(userBasketManagement.retrieveListOfAllTapasOrdersInBasket(id).size()).isEqualTo(amount);
+  @When("^the user adds (\\d+) Tapas with id (\\d+) to the Basket with id (\\d+)$")
+  public void theUserAddsTapasWithIdToTheBasketWithId(int numberTapas, String tapasId, int basketId) {
+    userBasketManagement.addNewTapasOrderToBasket(basketId, new TapasOrder(tapasId, numberTapas));
+  }
+
+  @Then("^the total number of items in the Basket with id (\\d+) equals (\\d+)$")
+  public void theTotalNumberOfItemsInTheBasketWithIdEquals(int basketId, int totalNumber) {
+    assertThat(userBasketManagement.retrieveListOfAllTapasOrdersInBasket(basketId).stream().map(to -> to.getAmount()).reduce(0L, Long::sum))
+        .isEqualTo(totalNumber);
+  }
+
+  @Then("^the number of items with id (\\d+) in the Basket with id (\\d+) equals (\\d+)$")
+  public void theNumberOfDifferentItemsInTheBasketWithIdEquals(String tapasId, int basketId, int amount) {
+    assertThat(
+        userBasketManagement.retrieveListOfAllTapasOrdersInBasket(basketId)
+            .stream()
+            .filter(to -> to.getTapasId().equals(tapasId))
+            .map(to -> to.getAmount()).findFirst().orElse(0L)
+    ).isEqualTo(amount);
+  }
+
+  @Then("^the Total Cost for all the Tapas in the Basket with id (\\d+) equals (\\d+)$")
+  public void theTotalCostForAllTheTapasInTheBasketEquals(int basketId, int totalCost) {
+    assertThat(userBasketManagement.calculateCostForBasket(basketId)).isEqualTo(BigDecimal.valueOf(totalCost));
   }
 
   @After
--- tapasapp/src/cucumber/resources/features/basketcreation.feature
+++ tapasapp/src/cucumber/resources/features/basketcreation.feature
@@ -1,4 +1,30 @@
 Feature: A new empty basket can be created
-  Scenario: Client creates a new basket, and verifies it's empty
-    When the user creates a new basket
-    Then the number of items in the basket with id 1 equals 0
+  Scenario: Client creates a new Basket, and verifies it's empty
+    When the user creates a new Basket
+    Then the total number of items in the Basket with id 1 equals 0
+
+  Scenario: Client can add a Tapas to his Basket
+    When the user creates a new Basket
+    And the user adds 5 Tapas with id 3 to the Basket with id 1
+    Then the number of items with id 3 in the Basket with id 1 equals 5
+    And the total number of items in the Basket with id 1 equals 5
+
+  Scenario: Client can add several Tapas to his Basket
+    When the user creates a new Basket
+    And the user adds 5 Tapas with id 3 to the Basket with id 1
+    And the user adds 3 Tapas with id 5 to the Basket with id 1
+    Then the number of items with id 3 in the Basket with id 1 equals 5
+    And the number of items with id 5 in the Basket with id 1 equals 3
+    And the total number of items in the Basket with id 1 equals 8
+
+  Scenario: Client can Calculate a Total Cost for all the Tapas in his Basket
+    When the user creates a new Basket
+    And the user adds 5 Tapas with id 3 to the Basket with id 1
+    And the user adds 3 Tapas with id 5 to the Basket with id 1
+    Then the Total Cost for all the Tapas in the Basket with id 1 equals 24
+
+  Scenario: Client can Add and then Remove Tapas from his Basket
+    When the user creates a new Basket
+    And the user adds 5 Tapas with id 3 to the Basket with id 1
+    And the user adds 3 Tapas with id 5 to the Basket with id 1
+    Then the Total Cost for all the Tapas in the Basket with id 1 equals 24
