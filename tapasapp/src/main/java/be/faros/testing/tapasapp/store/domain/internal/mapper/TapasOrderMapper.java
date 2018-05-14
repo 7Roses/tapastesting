f7a0073ac424c7aded7bba7ba7890d89fa76210c
Kristof Van Sever <vanseverk@gmail.com>
2018-05-14 18:16:58 +0200

    experimental -> testing

---
+++ tapasapp/src/main/java/be/faros/testing/tapasapp/store/domain/internal/mapper/TapasOrderMapper.java
@@ -0,0 +1,19 @@
+package be.faros.testing.tapasapp.store.domain.internal.mapper;
+
+import be.faros.testing.tapasapp.store.domain.internal.entity.TapasOrderEntity;
+import be.faros.testing.tapasapp.store.domain.usecases.dto.TapasOrder;
+import be.faros.testing.tapasapp.store.domain.usecases.dto.TapasOrder;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TapasOrderMapper {
+
+  public List<TapasOrder> map(List<TapasOrderEntity> tapasOrders) {
+    return tapasOrders.stream().map(this::map).collect(Collectors.toList());
+  }
+
+  public TapasOrder map(TapasOrderEntity tapasOrderEntity) {
+    return new TapasOrder(tapasOrderEntity.getTapasId(), tapasOrderEntity.getAmount());
+  }
+
+}
---
+++ tapasapp/src/main/java/be/faros/testing/tapasapp/store/domain/usecases/UserBasketManagement.java
@@ -0,0 +1,18 @@
+package be.faros.testing.tapasapp.store.domain.usecases;
+
+import be.faros.testing.tapasapp.store.domain.usecases.dto.TapasOrder;
+import be.faros.testing.tapasapp.store.domain.usecases.dto.TapasOrder;
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface UserBasketManagement {
+
+  int createNewBasket();
+
+  List<TapasOrder> retrieveListOfAllTapasOrdersInBasket(int basketId);
+
+  void changeTapasOrderInBasket(int basketId, TapasOrder tapasOrder);
+
+  BigDecimal calculateCostForBasket(int basketId);
+
+}
