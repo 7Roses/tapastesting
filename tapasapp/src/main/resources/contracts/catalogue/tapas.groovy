693f6e73595aea49019034b7d43064896c37c8a9
sterkje <jeroen.sterken@faros.be>
2018-05-15 13:06:07 +0200

    commit

--- tapasapp/src/main/resources/contracts/catalogue/tapas.groovy
+++ tapasapp/src/main/resources/contracts/catalogue/tapas.groovy
@@ -6,31 +6,10 @@ Contract.make{
     description "should return a list of all tapas"
 
     request{
-        method GET()
-        url "/tapas"
+
     }
 
     response{
-        status 200
-        headers {
-            header('Content-Type': value(
-                    producer(regex('application/json.*')),
-                    consumer('application/json')
-            ))
-        }
-        body (
-            [
-                [
-                        id: 0,
-                        name: "All i oli",
-                        price: 1.5
-                ],
-                [
-                        id: 1,
-                        name: "Banderillas",
-                        price: 3
-                ]
-             ]
-        )
+
     }
 }
