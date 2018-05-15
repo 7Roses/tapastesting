b0e16e37fc0139c6aa41ef987c7c42628e567947
sterkje <jeroen.sterken@faros.be>
2018-05-15 12:26:37 +0200

    commit

--- tapasapp/src/main/resources/contracts/catalogue/tapas.groovy
+++ tapasapp/src/main/resources/contracts/catalogue/tapas.groovy
@@ -3,7 +3,7 @@ import org.springframework.cloud.contract.spec.Contract
     TODO step3: create the contract (producer side)
 */
 Contract.make{
-    description "should return a list of all catalogue"
+    description "should return a list of all tapas"
 
     request{
         method GET()
