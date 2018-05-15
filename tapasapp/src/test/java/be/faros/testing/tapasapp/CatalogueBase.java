cb9f502f3161e9bb5857b9c9fe3d0fa9496686c3
sterkje <jeroen.sterken@faros.be>
2018-05-15 15:38:36 +0200

    commit

--- tapasapp/src/test/java/be/faros/testing/tapasapp/CatalogueBase.java
+++ tapasapp/src/test/java/be/faros/testing/tapasapp/CatalogueBase.java
@@ -34,7 +34,8 @@ public abstract class CatalogueBase {
         List<Tapas> tapas = Arrays.asList(new Tapas("0", "All i oli", new BigDecimal(1.5)),
                                           new Tapas("1", "Banderillas", new BigDecimal(3)));
 
-        Mockito.when(catalogueSearching.getAllTapas())
-                .thenReturn(tapas);
+        /*
+            TODO step8: use Mockito to return the list of tapas above when calling catalogueSearching.getAllTapas()
+         */
     }
 }
